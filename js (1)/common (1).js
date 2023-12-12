ac03e1b0bb92beafc706ef08a94147d4e7ac0370
SandyHuang <105573118+HuangHsinTzu@users.noreply.github.com>
2023-12-12 18:02:21 +0800

    Add files via upload

---
+++ js (1)/common (1).js
@@ -0,0 +1,21 @@
+
+.align-items-center h1>a{
+    font-size: 24px;
+}
+#form1{
+    width: 450px;
+    height: 400px;
+    border: 1px solid black;
+}
+th,td{
+    text-align: center;
+}
+td>a{
+    text-align: right;
+}
+#mainFrame{
+    background-color: gray;
+}
+a{
+    text-decoration:none;
+}
